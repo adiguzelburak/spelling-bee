4788978bea186316ce9e93677ef90d41940bb3b6
unknown <adiguzelburak@icloud.com>
2024-05-13 13:40:45 +0300

    letter, delete, shuffle buttons are ready.

---
+++ src/components/letter-button.tsx
@@ -0,0 +1,100 @@
+import { cn } from "@/lib/utils";
+import { Hexagon, Shuffle } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import Letter from "./letter";
+import { Button } from "./ui/button";
+
+interface ButtonProps {
+  middleLetter?: string;
+  letters?: string;
+  answers?: string[];
+  onChangeWord: (word: string) => void;
+}
+
+export default function LetterButtons({
+  middleLetter,
+  letters,
+  onChangeWord,
+}: ButtonProps) {
+  const [words, setWords] = useState<string[]>([]);
+  const [value, setValue] = useState<string>("");
+  const text = "selamik";
+  const mid = "e";
+
+  useEffect(() => {
+    shuffle();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [middleLetter, letters]);
+
+  const shuffleLetters = (letters: string[]) => {
+    let currentIndex = letters.length;
+
+    while (currentIndex != 0) {
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [letters[currentIndex], letters[randomIndex]] = [
+        letters[randomIndex],
+        letters[currentIndex],
+      ];
+    }
+  };
+
+  useEffect(() => {
+    onChangeWord(value);
+  }, [value, onChangeWord]);
+
+  const updateWord = (letter: string) => {
+    setValue((prevValue) => prevValue + letter);
+  };
+
+  const deleteLetter = () => {
+    if (value.length > 0) {
+      setValue((prevValue) => prevValue.slice(0, -1));
+    }
+  };
+
+  const shuffle = () => {
+    const splittedText = text?.split("");
+    const withoutMidLetter = splittedText.filter((lett) => lett !== mid);
+    shuffleLetters(withoutMidLetter);
+    setWords(withoutMidLetter);
+  };
+
+  return (
+    <div>
+      <div className="relative h-[400px] w-[350px]">
+        <div className="absolute top-0 left-32">
+          <Letter letter={words[0]} onLetterClicked={updateWord} />
+        </div>
+        <div className="absolute top-14 left-10">
+          <Letter letter={words[1]} onLetterClicked={updateWord} />
+        </div>
+        <div className="absolute top-28 left-32">
+          <Letter isMid letter={mid} onLetterClicked={updateWord} />
+        </div>
+        <div className="absolute top-14 left-[216px]">
+          <Letter letter={words[2]} onLetterClicked={updateWord} />
+        </div>
+        <div className="absolute top-40 left-10">
+          <Letter letter={words[3]} onLetterClicked={updateWord} />
+        </div>
+        <div className="absolute top-40 left-[216px]">
+          <Letter letter={words[4]} onLetterClicked={updateWord} />
+        </div>
+        <div className="absolute top-[216px] left-32">
+          <Letter letter={words[5]} onLetterClicked={updateWord} />
+        </div>
+        <div className="absolute bottom-0 left-1/2 -translate-x-1/2">
+          <div className="flex items-center space-x-3">
+            <Button onClick={deleteLetter}>Delete</Button>
+            <Button onClick={shuffle} size="icon">
+              <Shuffle className="w-[1.2rem] h-[1.2rem]" />
+            </Button>
+            <Button>Enter</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/letter.tsx
@@ -0,0 +1,29 @@
+import { cn } from "@/lib/utils";
+import { Hexagon } from "lucide-react";
+import React from "react";
+
+interface LetterButton {
+  isMid?: boolean;
+  letter: string;
+  onLetterClicked: (word: string) => void;
+}
+
+export default function Letter({
+  isMid,
+  letter,
+  onLetterClicked,
+}: LetterButton) {
+  return (
+    <div className="relative w-fit" onClick={() => onLetterClicked(letter)}>
+      <Hexagon
+        className={cn(
+          "rotate-90 h-28 w-28 stroke-0",
+          isMid ? "fill-yellow-400" : "fill-gray-200"
+        )}
+      />
+      <div className="absolute top-10 left-11 text-3xl font-semibold uppercase">
+        {letter}
+      </div>
+    </div>
+  );
+}
