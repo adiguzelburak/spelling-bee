de24d0155194b25296a32edfd3f70009fcdf331c
unknown <adiguzelburak@icloud.com>
2024-05-14 17:20:49 +0300

    language changes

--- src/pages/[lang].tsx
+++ src/pages/[lang].tsx
@@ -1,13 +1,18 @@
 import LetterButtons from "@/components/letter-button";
 import Timer from "@/components/timer";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Toaster } from "@/components/ui/toaster";
 import { DataType } from "@/lib/types";
 import { Gauge, WholeWord } from "lucide-react";
 import { GetServerSideProps } from "next";
-import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export default function Play({ repo }: { repo: DataType }) {
+  const router = useRouter();
   const [value, setValue] = useState("");
   const [timer, setTimer] = useState(0);
   const [totalPoint, setTotalPoint] = useState<number>(0);
@@ -38,9 +43,15 @@ export default function Play({ repo }: { repo: DataType }) {
     return total;
   };
 
+  useEffect(() => {
+    localStorage.setItem("correctAnswers", "");
+    setCorrectAnswers([]);
+    setTotalPoint(0);
+  }, [router.query.lang]);
+
   return (
     <div className="w-[400px]  px-4">
-      <div className=" grid grid-cols-2 gap-1">
+      <div className=" grid grid-cols-3 gap-1">
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Score</CardTitle>
@@ -56,7 +67,27 @@ export default function Play({ repo }: { repo: DataType }) {
           totalPoint={totalPoint}
           timeFinish={() => null}
         />
-        <Card className="col-span-2">
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">Language</CardTitle>
+          </CardHeader>
+          <CardContent className="grid grid-cols-2 gap-2">
+            <Link
+              target="_parent"
+              className="p-0"
+              href={router.query.lang === "en" ? "/tr" : "/en"}
+            >
+              <Image
+                src={router.query.lang === "en" ? "/tr.png" : "/us.png"}
+                alt="flag"
+                width={40}
+                height={20}
+                className="w-fit h-5 mt-2"
+              />
+            </Link>
+          </CardContent>
+        </Card>
+        <Card className="col-span-3">
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Words</CardTitle>
             <WholeWord className="h-4 w-4 text-muted-foreground" />
@@ -64,10 +95,7 @@ export default function Play({ repo }: { repo: DataType }) {
           <CardContent>
             <div className="text-base font-bold grid grid-cols-6">
               {correctAnswers.map((word, index) => (
-                <div key={index}>
-                  {word}
-                  {correctAnswers.length - 1 ? "," : ""}
-                </div>
+                <div key={index}>{word} ,</div>
               ))}
             </div>
           </CardContent>
